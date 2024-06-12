7a6be3059fb867466dbc7fb39829431a9660b878
sanjaychirpn <ssingh1@chirpn.com>
2024-06-12 11:56:36 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -9,6 +9,9 @@ router.get('/:id', authMiddleware, projectController.getProjectById);
 router.post('/', authMiddleware, adminMiddleware, projectController.createProject);
 router.put('/:id', authMiddleware, adminMiddleware, projectController.updateProject);
 router.delete('/:id', authMiddleware, adminMiddleware, projectController.deleteProject);
+// Add these routes to your existing router
 router.post('/assign', authMiddleware, adminMiddleware, projectController.assignProject);
+router.get('/assign/:userid', authMiddleware, projectController.getAssignedProjectsByUser);
+router.post('/unassign', authMiddleware, adminMiddleware, projectController.unassignProject);
 
 module.exports = router;
